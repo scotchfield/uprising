4107513ff50b4947a0c1db077eb8d67a0c099c0f
scotchfield <swrittenb@gmail.com>
2014-07-14 22:56:33 -0400

    More sidebar work. Add CSS from Bootstrap template, clean php echo.

--- custom-core.php
+++ custom-core.php
@@ -52,134 +52,134 @@ add_action( 'select_character', 'cr_login' );
 function cr_header() {
     global $game, $user, $character;
 
-    $obj = array( '<!DOCTYPE html>',
-'<html lang="en">',
-'  <head>',
-'    <meta charset="utf-8">',
-'    <meta http-equiv="X-UA-Compatible" content="IE=edge">',
-'    <meta name="viewport" content="width=device-width, initial-scale=1">',
-'    <title>' . GAME_NAME . ' (' . $game->get_action() . ')</title>',
-'    <link rel="stylesheet" href="' . GAME_URL . 'style/bootstrap.min.css">',
-'    <link rel="stylesheet" href="' . GAME_CUSTOM_STYLE_URL . 'cr.css">',
-'    <link href="http://fonts.googleapis.com/css?' .
-         'family=Raleway:400,500" rel="stylesheet" type="text/css">',
-'  </head>',
-'  <body>',
-'    <div id="popup" class="invis"></div>',
-'    <div class="navbar navbar-default navbar-fixed-top" role="navigation">',
-'      <div class="container">',
-'        <div class="navbar-header">',
-'          <button type="button" class="navbar-toggle" data-toggle=' .
-               '"collapse" data-target=".navbar-collapse">',
-'            <span class="sr-only">Toggle navigation</span>',
-'            <span class="icon-bar"></span>',
-'            <span class="icon-bar"></span>',
-'            <span class="icon-bar"></span>',
-'          </button>',
-'          <a class="navbar-brand" href="' . GAME_URL . '">' .
-               GAME_NAME . '</a>',
-'        </div>'
-        );
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo( GAME_NAME ); ?> (<?php echo( $game->get_action() );
+        ?>)</title>
+    <link rel="stylesheet" href="<?php echo( GAME_URL );
+        ?>style/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo( GAME_CUSTOM_STYLE_URL );
+        ?>cr.css">
+    <link href="http://fonts.googleapis.com/css?family=Raleway:400,500"
+          rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div id="popup" class="invis"></div>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle"
+                  data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?php echo( GAME_URL ); ?>"><?php
+              echo( GAME_NAME ); ?></a>
+        </div>
+<?php
 
     if ( FALSE != $character ) {
-        array_push( $obj,
-'        <div class="collapse navbar-collapse">',
-'          <ul class="nav navbar-nav">',
-'            <li class="dropdown">',
-'              <a href="#" class="dropdown-toggle" data-toggle="dropdown">' .
-                   'Navigate <b class="caret"></b></a>',
-'              <ul class="dropdown-menu">',
-'                <li class="dropdown-header">Main Locations</li>',
-'                <li><a href="?action=zone&amp;zone_tag=cydonia">' .
-                     'Cydonia Heavy Industries</a></li>',
-'                <li><a href="?action=zone&amp;zone_tag=minstall">' .
-                     'Mech Installations</a></li>',
-'                <li><a href="?action=zone&amp;zone_tag=cityhall">' .
-                     'City Hall</a></li>',
-'                <li><a href="?action=zone&amp;zone_tag=wordtruth">' .
-                     'The Word of Truth</a></li>',
-'                <li class="divider"></li>',
-'                <li class="dropdown-header">Combat Locations</li>',
-'                <li><a href="?action=zone&amp;zone_tag=titanrift">' .
-                     'Titan\'s Rift</a></li>',
-'                <li><a href="?action=zone&amp;zone_tag=epsilon">' .
-                     'The Epsilon Rift</a></li>',
-'              </ul>',
-'            </li>',
-'            <li><a href="?action=about">About</a></li>',
-'            <li><a href="?action=contact">Contact</a></li>',
-'          </ul>',
-'          <ul class="nav navbar-nav navbar-right">',
-'            <li class="dropdown">',
-'              <a href="#" class="dropdown-toggle" data-toggle="dropdown">' .
-                   $character[ 'character_name' ] .
-                   ' <b class="caret"></b></a>',
-'              <ul class="dropdown-menu">',
-'                <li><a href="?action=profile">Profile</a></li>',
-'                <li><a href="?action=questlog">Quest Log</a></li>',
-'                <li><a href="?action=mech">Active Mech</a></li>',
-'                <li><a href="?action=inventory">Inventory</a></li>',
-'                <li class="divider"></li>',
-'                <li><a href="?action=dashboard">Dashboard</a></li>',
-'                <li class="divider"></li>',
-'                <li><a href="game-setting.php?setting=change_character">' .
-                     'Change Character</a></li>',
-'                <li><a href="game-logout.php">Log out</a></li>',
-'              </ul>',
-'            </li>',
-'          </ul>',
-'        </div><!--/.nav-collapse -->'
-            );
+?>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                   Navigate <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li class="dropdown-header">Main Locations</li>
+                <li><a href="?action=zone&amp;zone_tag=cydonia">
+                     Cydonia Heavy Industries</a></li>
+                <li><a href="?action=zone&amp;zone_tag=minstall">
+                     Mech Installations</a></li>
+                <li><a href="?action=zone&amp;zone_tag=cityhall">
+                     City Hall</a></li>
+                <li><a href="?action=zone&amp;zone_tag=wordtruth">
+                     The Word of Truth</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Combat Locations</li>
+                <li><a href="?action=zone&amp;zone_tag=titanrift">
+                     Titan's Rift</a></li>
+                <li><a href="?action=zone&amp;zone_tag=epsilon">
+                     The Epsilon Rift</a></li>
+              </ul>
+            </li>
+            <li><a href="?action=about">About</a></li>
+            <li><a href="?action=contact">Contact</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php
+                  echo( $character[ 'character_name' ] );
+                  ?><b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="?action=profile">Profile</a></li>
+                <li><a href="?action=questlog">Quest Log</a></li>
+                <li><a href="?action=mech">Active Mech</a></li>
+                <li><a href="?action=inventory">Inventory</a></li>
+                <li class="divider"></li>
+                <li><a href="?action=dashboard">Dashboard</a></li>
+                <li class="divider"></li>
+                <li><a href="game-setting.php?setting=change_character">
+                    Change Character</a></li>
+                <li><a href="game-logout.php">Log out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+<?php
     }
+?>
+      </div>
+    </div>
 
-    array_push( $obj,
-'      </div>',
-'    </div>',
-'',
-'    <div class="container">',
-'      <div class="row">',
-'        <div class="col-sm-3 col-md-3 sidebar">',
-'          <ul class="nav nav-sidebar">',
-'            <li class="active"><a href="#">Overview</a></li>',
-'            <li><a href="#">Profile</a></li>',
-'            <li><a href="#">Inventory</a></li>',
-'            <li><a href="#">City</a></li>',
-'          </ul>',
-'          <ul class="nav nav-sidebar">',
-'            <li><a href="">Commit a Crime</a></li>',
-'            <li><a href="">Career</a></li>',
-'            <li><a href="">Fitness</a></li>',
-'            <li><a href="">Education</a></li>',
-'            <li><a href="">Casino</a></li>',
-'            <li><a href="">Jail</a></li>',
-'          </ul>',
-/*'          <ul class="nav nav-sidebar">',
-'            <li><a href="">Nav item again</a></li>',
-'            <li><a href="">One more nav</a></li>',
-'            <li><a href="">Another nav item</a></li>',
-'          </ul>',*/
-'        </div>',
-'        <div class="col-sm-9 main">'
-        );
+    <div class="container">
+      <div class="row">
 
-    echo join( "\n", $obj );
+<?php
+    if ( FALSE != $character ) {
+?>
+        <div class="col-sm-2 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Inventory</a></li>
+            <li><a href="#">City</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Commit a Crime</a></li>
+            <li><a href="">Career</a></li>
+            <li><a href="">Fitness</a></li>
+            <li><a href="">Education</a></li>
+            <li><a href="">Casino</a></li>
+            <li><a href="">Jail</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-10 col-sm-offset-2 main">
+<?php
+    }
 }
 
 function cr_footer() {
     global $game, $character;
 
-    $footer_text = '';
-
-    echo '      </div>';
-    echo '    </div></div>';
-    echo '    <div id="footer"><div class="container">';
-    echo '<p class="text-muted">' . $footer_text . '</p>';
-    echo '    </div></div>';
-    echo '<script src="' . GAME_URL . 'style/popup.js"></script>';
-    echo '<script src="' . GAME_URL . 'style/jquery.min.js"></script>';
-    echo '<script src="' . GAME_URL . 'style/bootstrap.min.js"></script>' .
-         "\n";
-    echo '</body></html>';
+    if ( FALSE != $character ) {
+        echo "      </div>\n";
+    }
+?>
+    </div>
+  </div>
+  <script src="<?php echo( GAME_URL ); ?>style/popup.js"></script>
+  <script src="<?php echo( GAME_URL ); ?>style/jquery.min.js"></script>
+  <script src="<?php echo( GAME_URL ); ?>style/bootstrap.min.js"></script>
+  </body>
+</html>
+<?php
 }
 
 add_action( 'game_header', 'cr_header' );
