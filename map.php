e7ed716dd281945577747b626363ea3ef5080dad
scotchfield <swrittenb@gmail.com>
2014-08-05 09:29:02 -0400

    Remove old Shattered City zone information from map.php.

--- map.php
+++ map.php
@@ -12,36 +12,7 @@ function cr_map_print() {
   <h1 class="page_section">State of the City</h1>
 </div>
 <div class="row">
-  <div class="col-md-4">
-    <h4>Primary Locations</h4>
-    <ul>
-      <li><a href="?action=zone&zone_tag=cydonia">Cydonia
-          Heavy Industries</a></li>
-      <li><a href="?action=zone&zone_tag=minstall">Mech Installations</a></li>
-    </ul>
-    <h4>Goods and Services</h4>
-    <ul>
-      <li><a href="?action=zone&zone_tag=mequip">Mech
-          Equipment Storeroom</a></li>
-    </ul>
-    <h4>Recreational Activities</h4>
-    <ul>
-      <li><a href="?action=zone&zone_tag=casino">City Casino</a></li>
-    </ul>
-  </div>
-  <div class="col-md-4">
-    <h4>Combat Locations</h4>
-    <ul>
-      <li><a href="?action=zone&zone_tag=titanrift">Titan's Rift</a></li>
-      <li><a href="?action=zone&zone_tag=epsilon">The Epsilon Rift</a></li>
-    </ul>
-  </div>
-  <div class="col-md-4">
-    <h4>Helpful Links</h4>
-    <ul>
-      <li><a href="?action=allquests">Find all available quests</a></li>
-    </ul>
-  </div>
+
 </div>
 <?php
 }
